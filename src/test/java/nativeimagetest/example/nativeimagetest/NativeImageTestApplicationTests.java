c1a0ef4ed1f9babd9a1654ec4a1a1d3f3f8bf296
Fenqin Zhou <fenzho@microsoft.com>
2023-05-12 10:40:15 +0800

    initial commit

---
+++ src/test/java/nativeimagetest/example/nativeimagetest/NativeImageTestApplicationTests.java
@@ -0,0 +1,13 @@
+package nativeimagetest.example.nativeimagetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeImageTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
